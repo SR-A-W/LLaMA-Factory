5dde185e8107ef9c049a6721ae556e85119db0a0
SR-A-W <adamwang976@gmail.com>
2025-08-25 16:24:38 -0400

    Added HT SFT settings

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -1810,6 +1810,19 @@ register_template(
     ),
 )
 
+register_template(
+    name="qwen25",
+    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
+    format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
+    format_observation=StringFormatter(slots=["<|im_start|>tool\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
+    format_separator=EmptyFormatter(slots=["\n"]),
+    default_system="You are Qwen, created by Alibaba Cloud. You are a helpful assistant.",
+    stop_words=["<|endoftext|>"],
+    replace_eos=True,
+    replace_jinja_template=False,
+)
+
+
 # copied from qwen template
 register_template(
     name="qwen2_vl",
